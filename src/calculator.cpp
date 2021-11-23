04f02bb8a836582a052f784b2526c18cab49c763
danie <daniel.tianyu.han@gmail.com>
2021-11-23 18:01:12 -0500

    first commit

---
+++ src/calculator.cpp
@@ -0,0 +1,21 @@
+#include "calculator.h"
+
+//add
+int myCustomAdd(int a, int b) {
+    return a+b;
+};
+
+//subtract
+int myCustomSubtract(int a, int b) {
+    return a-b;
+}
+
+//multiply
+int myCustomMultiply(int a, int b) {
+    return a*b;
+}
+
+//divide
+int myCustomDivide(int a, int b) {
+    return a/b;
+}
